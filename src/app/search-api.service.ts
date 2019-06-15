46dd091905543b0bc20496096c0e1e1ffa7d5e5f
Heather Wenzel <hcwenzel@gmail.com>
2019-06-15 19:01:05 -0400

    final styling

--- src/app/search-api.service.ts
+++ src/app/search-api.service.ts
@@ -10,14 +10,17 @@ export class SearchApiService {
 
   constructor(private http: HttpClient) { }
 
+  //makes the initial api call for 5 movie trailers on window load
   loadYouTube() {
     return this.http.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=amazon%20studios%20trailer&type=video&key=${this.apikey}`);
   }
 
+  //makes the search api call based on the user's search term
   getSearchData(usersearch: string) {
     return this.http.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${usersearch}&type=video&key=${this.apikey}`);
   }
 
+  //makes the data api call based on the videoid pulled from the search api call
   getVideoData(videoid: string) {
     return this.http.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoid}&key=${this.apikey}`);
   }
--- src/app/youtube/youtube.component.ts
+++ src/app/youtube/youtube.component.ts
@@ -13,40 +13,50 @@ export class YoutubeComponent implements OnInit {
   showThree: boolean = false;
   showFour: boolean = false;
   showFive: boolean = false;
-  searchResults: any; //contains all of the search result responses
+  searchResults: any; //contains all of the search api call responses
   searchArray: any[] = []; //contains an array of all the search results
   videoids: any[] = []; //contains an array of all of the search responses' ids
   videoid: string; //holds the current videoid
-  videoData: any; //contains the data response
+  videoData: any; //contains all of the data api call responses
 
   constructor(private apiService: SearchApiService, private sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
+    //loads the initial search api call on window load
     this.apiService.loadYouTube().subscribe(response => {
       this.searchResults = response;
-            for (let i=0; i<5; i++) {
+      //pushes all of the videoids from the search api call into the videoids array
+      for (let i=0; i<5; i++) {
         this.videoids.push(this.searchResults.items[i].id.videoId);
       }
+      //sets the initial videoid to the first video in the search results
       this.videoid = this.videoids[0];
+      //makes the initial data api call
       this.apiService.getVideoData(this.videoid).subscribe(response => {
         this.videoData = response;
       });
+      //shows the first video with information on load
       this.showOne = !this.showOne;
     });
   }
 
+  //calls the search api and data api when the search button is clicked, taking in the user input as its parameter
   async getSearchResults(form) {
+    //resets the array of videoids
     this.videoids.length = 0;
+    //calls the search api
     this.searchResults = await this.apiService.getSearchData(form.value.search).toPromise();
     this.searchArray = this.searchResults.items;
-    console.log(this.searchArray);
+    //adds all of the videoids from the search api call into the videoids array
     for (let i=0; i<5; i++) {
       this.videoids.push(this.searchResults.items[i].id.videoId);
     }
+    //shows the first video with its corresponding information
     this.showOne = true;
   }
 
+  //shows video one when its corresponding button is clicked, and hides the other videos and data if they are showing
   videoOne($event) {
     this.showOne = !this.showOne;
     this.showTwo = false;
@@ -59,10 +69,12 @@ export class YoutubeComponent implements OnInit {
     });
   }
 
+  //sanitizes the url for video one so it can be inserted in the iframe
   getVideoOne() {
     return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.videoids[0]);
   }
 
+  //shows video two when its corresponding button is clicked, and hides the other videos and data if they are showing
   videoTwo($event) {
     this.showTwo = !this.showTwo;
     this.showOne = false;
@@ -75,10 +87,12 @@ export class YoutubeComponent implements OnInit {
     });
   }
 
+  //sanitizes the url for video two so it can be inserted in the iframe
   getVideoTwo() {
     return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.videoids[1]);
   }
 
+  //shows video three when its corresponding button is clicked, and hides the other videos and data if they are showing
   videoThree($event) {
     this.showThree = !this.showThree;
     this.showOne = false;
@@ -91,10 +105,12 @@ export class YoutubeComponent implements OnInit {
     });
   }
 
+  //sanitizes the url for video three so it can be inserted in the iframe
   getVideoThree() {
     return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.videoids[2]);
   }
-
+  
+  //shows video four when its corresponding button is clicked, and hides the other videos and data if they are showing
   videoFour($event) {
     this.showFour = !this.showFour;
     this.showOne = false;
@@ -107,10 +123,12 @@ export class YoutubeComponent implements OnInit {
     });
   }
 
+  //sanitizes the url for video four so it can be inserted in the iframe
   getVideoFour() {
     return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.videoids[3]);
   }
 
+  //shows video five when its corresponding button is clicked, and hides the other videos and data if they are showing
   videoFive($event) {
     this.showFive = !this.showFive;
     this.showOne = false;
@@ -123,6 +141,7 @@ export class YoutubeComponent implements OnInit {
     });
   }
 
+  //sanitizes the url for video five so it can be inserted in the iframe
   getVideoFive() {
     return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.videoids[4]);
   }
