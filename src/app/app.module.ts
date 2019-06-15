218d4259942712968a777b4dce35e63de567656f
Heather Wenzel <hcwenzel@gmail.com>
2019-06-15 12:28:45 -0400

    mobile styling completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { YoutubeComponent } from './youtube/youtube.component';
 import { SearchApiService } from './search-api.service';
 import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,7 @@ import { FormsModule } from '@angular/forms';
     YoutubeComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, FormsModule
+    BrowserModule, HttpClientModule, FormsModule, NoopAnimationsModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule
   ],
   providers: [SearchApiService],
   bootstrap: [AppComponent]
